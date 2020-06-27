39a8245021d5c5c2f3dd2f570abdbe6bb2a5fa5e
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-06-26 23:13:20 -0400

    Added api router for later use on updating user info

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
@@ -12,6 +13,14 @@ urlpatterns = [
     # user management
     path("accounts/", include("allauth.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# API URLS
+urlpatterns += [
+    # API base url
+    path("api/", include("config.api_router")),
+    # DRF auth token
+    path("auth-token/", obtain_auth_token)
+]
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
