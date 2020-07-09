e6a658ead33514ab1f2547806b3251aff1660e10
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-07-08 23:22:21 -0400

    Removed additional columns from serializer since this serializer has not been updated yet

--- cdsso/users/tests/test_drf_views.py
+++ cdsso/users/tests/test_drf_views.py
@@ -1,13 +1,10 @@
 import pytest
-from datetime import datetime
-
 from django.test import RequestFactory
 
 from cdsso.users.api.views import UserViewSet
 from cdsso.users.models import User
 
 pytestmark = pytest.mark.django_db
-DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 
 class TestUserViewSet:
@@ -34,20 +31,4 @@ class TestUserViewSet:
             "email": user.email,
             "name": user.name,
             "url": f"http://testserver/api/users/{user.username}/",
-            "anonymous": user.anonymous,
-            "bio": user.bio,
-            "date_joined": datetime.strftime(user.date_joined),
-            "dob": user.dob,
-            "facebook_url": user.facebook_url,
-            "first_name": user.first_name,
-            "github_username": user.github_username,
-            "id": user.id,
-            "instagram_url": user.instagram_url,
-            "is_active": user.is_active,
-            "last_login": datetime.strftime(user.last_login, fmt=DATE_FORMAT),
-            "last_name": user.last_name,
-            "linkedin_url": user.linkedin_url,
-            "receive_notifications": user.receive_notifications,
-            "slack_username": user.slack_username,
-            "twitter_username": user.twitter_username
         }
