11328f2f55cf6a5accbfb7268efd5f3ec2d736fc
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-07-08 23:41:43 -0400

    i18n url patterns added to tests

--- cdsso/users/tests/test_views.py
+++ cdsso/users/tests/test_views.py
@@ -49,7 +49,7 @@ class TestUserRedirectView:
 
         view.request = request
 
-        assert view.get_redirect_url() == f"/users/{user.username}/"
+        assert view.get_redirect_url() == f"/en-us/users/{user.username}/"
 
 
 class TestUserDetailView:
@@ -68,7 +68,7 @@ class TestUserDetailView:
         response = user_detail_view(request, username=user.username)
 
         assert response.status_code == 302
-        assert response.url == "/accounts/login/?next=/fake-url/"
+        assert response.url == "/en-us/accounts/login/?next=/fake-url/"
 
     def test_case_sensitivity(self, rf: RequestFactory):
         request = rf.get("/fake-url/")
