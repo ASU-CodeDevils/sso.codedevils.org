7fde262af1809018ae879e162bf3b781e119066d
KevinThePepper <kevin.shelley@pm.me>
2020-09-06 02:19:20 -0700

    Request skeleton for obtaining user info from Flameboi

--- cdsso/contrib/register/tasks.py
+++ cdsso/contrib/register/tasks.py
@@ -1,7 +1,9 @@
 import logging
+import requests
 from typing import List
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.core.mail import mail_managers
 from django.core.validators import validate_email
@@ -9,15 +11,22 @@ from django.core.validators import validate_email
 from config import celery_app
 
 from .models import StudentRegistration
+from .utils import get_flameboi_jwt
 
 logger = logging.getLogger()
 Emails = List[str]
+User = get_user_model()
 
 
 @celery_app.task()
 def register_on_slack(emails: Emails):
     """
-    Registers a single email or list of emails on Slack using Flameboi.
+    Since Flameboi cannot (as of the initial version of Flameboi and v1.0.1 of SSO) register members on Slack,
+    this simply checks Slack to see if the user has been registered yet or not. If the user is registered on
+    Slack, their information is automatically populated.
+
+    Args:
+        email (Emails): The list of emails to be checked.
     """
     if settings.REGISTER_SLACK_USERS_WITH_FLAMEBOI:
         # validate emails, this will raise a ValidationError
@@ -29,8 +38,51 @@ def register_on_slack(emails: Emails):
                 logger.warning(f"Invalid email to add to Slack: {email}")
                 invalid_emails.append(email)
 
-        # TODO implement Flameboi API
-        pass
+        # remove invalid emails
+        valid_emails = [email for email in emails if email not in invalid_emails]
+
+        # TODO check this endpoint
+        url = settings.FLAMEBOI_API_URL + "get_student/"
+
+        # get the access token needed for the requests
+        try:
+            access, _ = get_flameboi_jwt()
+        except (KeyError, requests.HTTPError):
+            logger.error("Failed to authenticate against Flameboi with JWT")
+
+        # for the valid emails, check against Slack for membership
+        for email in valid_emails:
+            response = requests.get(
+                url=url,
+                headers={"Authorization": "Bearer " + access, "Accept": "application/json"}
+            )
+            if response.status_code == 200:
+                data = response.json()
+                if data["ok"]:
+                    member = User.objects.get(email__exact=email)
+                    user = data["user"]
+                    # update the user
+                    member.slack_id = user["id"]
+                    member.tz_offset = user["tz_offset"]
+                    member.name = user["profile"]["real_name"]
+                    member.image_24 = user["profile"]["image_24"]
+                    member.image_512 = user["profile"]["image_512"]
+                    member.save()
+                    # update registration
+                    member.studentregistration.slack_registered = True
+                    member.studentregistration.save()
+                    logger.info("New member registered on Slack: {}".format(email))
+                # will error if the user has not been found
+                else:
+                    error = data["error"]
+                    if error == "user_not_found":
+                        logger.warning("User registered on SSO, but not on Slack: {}".format(email))
+                    else:
+                        logger.error("An unknown error occurred with the Flameboi API: {}".format(error))
+            else:
+                logger.error("Invalid status code {} when retrieving member info {}".format(
+                    response.status_code, email
+                ))
     else:
         logger.warning(
             "To add users to slack automatically, please set REGISTER_SLACK_USERS_WITH_FLAMEBOI to True"
