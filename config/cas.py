c6478c7564f812b781b7b638ca2a75f35c359fe0
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-10-16 12:13:09 -0700

    Regex expressions on re_path urls

--- config/cas.py
+++ config/cas.py
@@ -1,5 +1,5 @@
 from cas_server import views
-from django.conf.urls import url, re_path
+from django.conf.urls import re_path
 from django.urls import path
 from django.views.decorators.debug import sensitive_post_parameters, sensitive_variables
 from django.views.generic import RedirectView
@@ -17,38 +17,38 @@ urlpatterns = [
     ),
     path("login/", sensitive_post_parameters("password")(sso_login_view), name="login"),
     path("logout/", sso_logout_view, name="logout"),
-    re_path("^validate$", views.Validate.as_view(), name="validate"),
+    re_path(r"^validate$", views.Validate.as_view(), name="validate"),
     re_path(
-        "^serviceValidate$",
+        r"^serviceValidate$",
         views.ValidateService.as_view(allow_proxy_ticket=False),
         name="serviceValidate",
     ),
     re_path(
-        "^proxyValidate$",
+        r"^proxyValidate$",
         views.ValidateService.as_view(allow_proxy_ticket=True),
         name="proxyValidate",
     ),
-    re_path("^proxy$", views.Proxy.as_view(), name="proxy"),
+    re_path(r"^proxy$", views.Proxy.as_view(), name="proxy"),
     re_path(
-        "^p3/serviceValidate$",
+        r"^p3/serviceValidate$",
         views.ValidateService.as_view(allow_proxy_ticket=False),
         name="p3_serviceValidate",
     ),
     re_path(
-        "^p3/proxyValidate$",
+        r"^p3/proxyValidate$",
         views.ValidateService.as_view(allow_proxy_ticket=True),
         name="p3_proxyValidate",
     ),
-    re_path("^samlValidate$", views.SamlValidate.as_view(), name="samlValidate"),
+    re_path(r"^samlValidate$", views.SamlValidate.as_view(), name="samlValidate"),
     re_path(
-        "^auth$",
+        r"^auth$",
         sensitive_variables("password", "secret")(
             sensitive_post_parameters("password", "secret")(views.Auth.as_view())
         ),
         name="auth",
     ),
     re_path(
-        "^federate(?:/(?P<provider>([^/]+)))?$",
+        r"^federate(?:/(?P<provider>([^/]+)))?$",
         views.FederateAuth.as_view(),
         name="federateAuth",
     ),
