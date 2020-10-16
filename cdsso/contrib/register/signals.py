5e9c04b6e20c31bd69d1eb9c02a5011b73019916
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-10-16 11:46:02 -0700

    Inserted notification flags to reduce the number of repetitive notifications

--- cdsso/contrib/register/signals.py
+++ cdsso/contrib/register/signals.py
@@ -35,29 +35,39 @@ def notify_complete_registration(instance: StudentRegistration, **kwargs):
     """
 
     if settings.RUN_REGISTRATION_POST_SAVE_SIGNAL:
+        save_again = False  # needed for if we notify admin/the user
 
         # add users to slack automatically with Flameboi util
         if (
-            settings.REGISTER_SLACK_USERS_WITH_FLAMEBOI
+            settings.FLAMEBOI["REGISTER_SLACK_USERS_WITH_FLAMEBOI"]
             and not instance.slack_registered
-            and not instance._slack_add_attempt
+            and not instance.slack_add_attempt
         ):
             register_on_slack.delay(emails=[instance.user.email])
-            instance._slack_add_attempt = True
+            instance.slack_add_attempt = True
+            save_again = True
 
         # notify managers of new users to be added to SunDevilSync
         if (
             settings.NOTIFY_MANAGERS_SDS_REGISTRATION
             and not instance.sds_registered
-            and not instance._sds_notified
+            and not instance.sds_notified
         ):
             notify_sds_registration.delay(instance.user.email)
-            instance._sds_notified = True
+            instance.sds_notified = True
+            save_again = True
 
         # notify a user if their registration has been completed
         if (
             settings.SEND_COMPLETED_REGISTRATION_NOTIFICATION
             and instance.completed_registration
+            and not instance.completed_registration_notification
+            and not self._restart_registration
         ):
             # TODO send the user an email saying their registration has been completed
             email_user_complete_registration(email=instance.user.email)
+            instance.completed_registration_notification = True
+            save_again = True
+
+        if save_again:
+            instance.save()
